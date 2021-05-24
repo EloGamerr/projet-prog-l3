57a124c17b7a6b46de0405266d6e16ce285d6aea
Glémont <clement.corbille@etu.univ-grenoble-alpes.fr>
2021-05-24 21:38:48 +0200

    Correctifs Audit Model

--- src/main/java/fr/prog/tablut/model/Game.java
+++ src/main/java/fr/prog/tablut/model/Game.java
@@ -6,26 +6,27 @@ import java.util.List;
 
 import fr.prog.tablut.structures.Couple;
 
+
 public class Game {
 	private int rowAmount, colAmount;
 	private CellContent[][] grid;
 	private final int middle;
 	private int kingL, kingC;
 	private PlayerEnum winner;
-	private final List<Couple<Integer, Integer>> visited = new ArrayList<>();
 	private PlayerEnum player;
+	private Move move;
 	
 	public Game(){
 		this.middle = 4;
 		this.player = PlayerEnum.ATTACKER;
-		
+		this.move = new Move(this);
 		init_game(9,9);
 	}
 
 	void init_game(int rowAmount, int colAmount) {
-		grid = new CellContent[rowAmount][colAmount];
+		setGrid(new CellContent[rowAmount][colAmount]);
 		for(int i = 0 ; i < rowAmount ; i++) {
-			Arrays.fill(grid[i], CellContent.EMPTY);
+			Arrays.fill(getGrid()[i], CellContent.EMPTY);
 		}
 		
 		this.rowAmount = rowAmount;
@@ -93,7 +94,7 @@ public class Game {
 
 		if(toL != l && toC != c) return false; // Déplacements en diagonal non autorisés
 
-		CellContent fromCellContent = grid[l][c];
+		CellContent fromCellContent = getGrid()[l][c];
 		if(fromCellContent == CellContent.EMPTY || fromCellContent == CellContent.GATE) return false;
 
 		List<Couple<Integer, Integer>> accessibleCells = accessibleCells(l, c);
@@ -105,20 +106,15 @@ public class Game {
 		result.move(l, c, toL, toC);
 		l = toL;
 		c = toC;
-		CellContent previousToCellContent = grid[l][c];
+		CellContent previousToCellContent = getGrid()[l][c];
 		setContent(fromCellContent, l, c);
 		
 		if(isAttackTower(l, c)) {
-			towerTaker_attack(l,c);
-			if(!visited.isEmpty()) {
-				visited.clear();
-			}
+			move.towerTaker_attack(l,c);
+
 		}
 		if(isDefenseTower(l, c)) {
-			towerTaker_defense(l,c);
-			if(!visited.isEmpty()) {
-				visited.clear();
-			}
+			move.towerTaker_defense(l,c);
 		}
 		if((winner = checkWin(previousToCellContent, fromCellContent)) != null) {
 			System.out.println(winner + " a gagné !");
@@ -161,8 +157,8 @@ public class Game {
 	}
 	
 	private boolean cantAccess(int fromL, int fromC, int toC, int toL, List<Couple<Integer, Integer>> accessibleCells) {
-		CellContent fromCellContent = grid[fromL][fromC];
-		CellContent toCellContent = grid[toL][toC];
+		CellContent fromCellContent = getGrid()[fromL][fromC];
+		CellContent toCellContent = getGrid()[toL][toC];
 		
 		if(toCellContent == CellContent.GATE || (isTheKingPlace(toC, toL) && toCellContent == CellContent.EMPTY)) {
 			if(fromCellContent == CellContent.KING) {
@@ -178,139 +174,7 @@ public class Game {
 		return false;
 	}
 	
-	public void towerTaker_attack(int l, int c) {
-		towerTaker_attack_core(l-1,c,-1,0);
-		towerTaker_attack_core(l+1,c,1,0);
-		towerTaker_attack_core(l,c-1,0,-1);
-		towerTaker_attack_core(l,c+1,0,1);
-	}
 	
-	public void towerTaker_defense(int l, int c) {
-		towerTaker_defense_core(l-1,c,-1,0);
-		towerTaker_defense_core(l+1,c,1,0);
-		towerTaker_defense_core(l,c-1,0,-1);
-		towerTaker_defense_core(l,c+1,0,1);
-	}
-	
-	// Prendre en compte les murs et portes
-	public void towerTaker_defense_core(int l, int c, int dl, int dc){
-		if(isAttackTower(l, c)){
-			if(isDefenseTower(l+dl, c+dc)){
-				setContent(CellContent.EMPTY,l,c);
-			}
-			
-			else if(isAttackTower(l+dl, c+dc)) {
-				if(isSurrounded(null,l,c) && isSurrounded(new Couple<Integer, Integer>(l, c),l+dl,c+dc)) {
-					setContent(CellContent.EMPTY,l,c);
-					setContent(CellContent.EMPTY,l+dl,c+dc);
-				}
-			}
-		}
-	}
-
-	// Faire l'attaque du roi + prendre en compte les murs et portes
-	public void towerTaker_attack_core(int l, int c, int dl, int dc){
-		if((isDefenseTower(l,c) || isTheKing(l,c)) && isADefenseAllied(l+dl, c+dc)) {
-			System.out.println("Entrée dans isSurrounded");
-			if(isSurrounded(null,l,c) || isSurrounded(new Couple<Integer, Integer>(l, c),l+dl,c+dc)) {
-				setContent(CellContent.EMPTY,l,c);
-				setContent(CellContent.EMPTY,l+dl,c+dc);
-			}
-		}
-		else if(isDefenseTower(l, c)){
-			if(isAttackTower(l+dl, c+dc)){
-				setContent(CellContent.EMPTY,l,c);
-			}
-		}
-		else if(isTheKing(l,c)){
-			if(isSurrounded(null,l,c)) {
-				setContent(CellContent.EMPTY,l,c);
-			}
-		}
-			 
-
-		}
-
-	public Couple<Integer, Integer> checkneighbour_attack(List<Couple<Integer, Integer>> allyCells, int counter_obstacle, int counter_enemy, int i, int j){
-		if(isADefenseAllied(i,j)) {
-			counter_obstacle++;
-			counter_enemy++;
-		}
-		else if(isOccupied(i, j)){
-			allyCells.add(new Couple<Integer, Integer>(i, j));
-			counter_obstacle++;
-		}
-		return new Couple<Integer, Integer>(counter_obstacle, counter_enemy);
-	}
-	
-	public Couple<Integer, Integer> checkneighbour_defense(List<Couple<Integer, Integer>> allyCells, int counter_obstacle, int counter_enemy, int i, int j){
-		if(isAnAttackAllied(i,j)) {
-			counter_obstacle++;
-			counter_enemy++;
-		}
-		else if(isOccupied(i, j)){
-			allyCells.add(new Couple<Integer, Integer>(i, j));
-			counter_obstacle++;
-		}
-		return new Couple<Integer, Integer>(counter_obstacle, counter_enemy);
-	}
-	
-	public boolean isSurrounded(Couple<Integer, Integer> c, int i, int j) {
-		List<Couple<Integer, Integer>> allyCells = new ArrayList<>();
-		boolean surrounded = true;
-		int counter_obstacle = 0;
-		int counter_enemy = 0;
-		Couple<Integer, Integer> nb = new Couple<Integer, Integer>(counter_obstacle, counter_enemy);
-		switch(grid[i][j]) {
-			case DEFENSE_TOWER:
-			case KING:
-
-				nb = checkneighbour_defense(allyCells, nb.getFirst(), nb.getSecond(), i-1, j);
-				nb = checkneighbour_defense(allyCells, nb.getFirst(), nb.getSecond(), i+1, j);
-				nb = checkneighbour_defense(allyCells, nb.getFirst(), nb.getSecond(), i, j-1);
-				nb = checkneighbour_defense(allyCells, nb.getFirst(), nb.getSecond(), i, j+1);
-				
-				break;
-
-			case ATTACK_TOWER:
-				
-				nb = checkneighbour_attack(allyCells, nb.getFirst(), nb.getSecond(), i-1, j);
-				nb = checkneighbour_attack(allyCells, nb.getFirst(), nb.getSecond(), i+1, j);
-				nb = checkneighbour_attack(allyCells, nb.getFirst(), nb.getSecond(), i, j-1);
-				nb = checkneighbour_attack(allyCells, nb.getFirst(), nb.getSecond(), i, j+1);
-				
-				break;
-				
-			default:
-				
-				break;	
-		}
-		
-		if(nb.getFirst() == 4) {
-			System.out.println("nb obstacles : "+nb.getFirst());
-			System.out.println("nb enemy : "+nb.getSecond());
-			if(nb.getSecond() == 4) {
-				return true;
-			}
-			else {
-				if(!visited.contains(c)) {
-					visited.add(c);
-				}
-				for(Couple<Integer, Integer> cell : visited) {
-					allyCells.remove(cell);
-				}
-				for(Couple<Integer, Integer> cell : allyCells) {
-					if(!isSurrounded(new Couple<Integer, Integer>(i, j),cell.getFirst(),cell.getSecond())) {
-						surrounded = false;
-					}
-				}
-			}
-		}
-		else 
-			return false;
-		
-		return surrounded;			
-	}
 	
 
 	
@@ -321,7 +185,7 @@ public class Game {
 			kingC = c;
 			kingL = l;
 		}
-		grid[l][c] = cellContent;
+		getGrid()[l][c] = cellContent;
 	}
 
 	public PlayerEnum getWinner() {
@@ -341,79 +205,64 @@ public class Game {
 	
 	
 	private void clear(int l, int c) {
-		grid[l][c] = CellContent.EMPTY;
+		getGrid()[l][c] = CellContent.EMPTY;
 		
 	}
 
 	public void setDefenseTower(int l, int c) {
-		grid[l][c] = CellContent.DEFENSE_TOWER;
+		getGrid()[l][c] = CellContent.DEFENSE_TOWER;
 	}
 	
 	public void setAttackTower(int l, int c) {
-		grid[l][c] = CellContent.ATTACK_TOWER;
+		getGrid()[l][c] = CellContent.ATTACK_TOWER;
 	}
 	
 	public void setKing(int l, int c) {
-		grid[l][c] = CellContent.KING;
+		getGrid()[l][c] = CellContent.KING;
 		kingC = c;
 		kingL = l;
 	}
 	
 	public void setGate(int l, int c) {
-		grid[l][c] = CellContent.GATE;
+		getGrid()[l][c] = CellContent.GATE;
 	}
 	
-	public boolean isAWall(int i, int j) {
-		return i == rowAmount - 1 || j == colAmount - 1;
-	}
 	
 	public boolean isTheKingPlace(int i, int j) {
 		return i == middle && j == middle;
 	}
 	
 	
-	
-	public boolean isADefenseAllied(int i, int j) {
-		if(isAWall(i,j)|| isTheKingPlace(i,j) || isAttackTower(i,j) || isGate(i,j))
-			return false;
-		else return isDefenseTower(i, j) || isTheKing(i, j);
-	}
-	
-	public boolean isAnAttackAllied(int i, int j) {
-		if(isAWall(i,j) ||isDefenseTower(i,j) || isTheKing(i,j)|| isTheKingPlace(i,j) ||  isGate(i,j)) {
-			return false;
-		}
-		else return isAttackTower(i, j);
-	}
+
 	
 	public boolean isOccupied(int l, int c) {
 		if(!isValid(l, c)) return true;
 		
-		return grid[l][c] != CellContent.EMPTY;
+		return getGrid()[l][c] != CellContent.EMPTY;
 	}
 
 	public boolean isTheKing(int l, int c) {
 		if(!isValid(l, c)) return false;
 		
-		return grid[l][c] == CellContent.KING;
+		return getGrid()[l][c] == CellContent.KING;
 	}
 	
 	public boolean isAttackTower(int l, int c) {
 		if(!isValid(l, c)) return false;
 		
-		return grid[l][c] == CellContent.ATTACK_TOWER;
+		return getGrid()[l][c] == CellContent.ATTACK_TOWER;
 	}
 	
 	public boolean  isDefenseTower(int l, int c) {
 		if(!isValid(l, c)) return false;
 		
-		return grid[l][c] == CellContent.DEFENSE_TOWER;
+		return getGrid()[l][c] == CellContent.DEFENSE_TOWER;
 	}
 	
 	public boolean isGate(int l, int c) {
 		if(!isValid(l, c)) return false;
 		
-		return grid[l][c] == CellContent.GATE;
+		return getGrid()[l][c] == CellContent.GATE;
 	}
 	
 	
@@ -426,7 +275,7 @@ public class Game {
 	}
 	
 	public CellContent getCellContent(int l, int c) {
-		return this.grid[l][c];
+		return this.getGrid()[l][c];
 	}
 	
 	public boolean isValid(int l, int c) {
@@ -443,5 +292,13 @@ public class Game {
 		}
 		
 		return this.getCellContent(l, c) == CellContent.DEFENSE_TOWER || this.getCellContent(l, c) == CellContent.KING;
+	}
+
+	public CellContent[][] getGrid() {
+		return grid;
+	}
+
+	public void setGrid(CellContent[][] grid) {
+		this.grid = grid;
 	}
 }
---
+++ src/main/java/fr/prog/tablut/model/Move.java
@@ -0,0 +1,178 @@
+package fr.prog.tablut.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import fr.prog.tablut.structures.Couple;
+
+public class Move {
+	private List<Couple<Integer, Integer>> visited = new ArrayList<>();
+	private Game game;
+	
+	
+	Move(Game game){
+		this.game = game;
+	}
+	
+	// On check les voisins du pion attaquant qui a joué
+	public void towerTaker_attack(int l, int c) {
+		towerTaker_attack_core(l-1,c,-1,0);
+		towerTaker_attack_core(l+1,c,1,0);
+		towerTaker_attack_core(l,c-1,0,-1);
+		towerTaker_attack_core(l,c+1,0,1);
+	}
+	// On check les voisins du pion attaquant qui a joué
+	public void towerTaker_defense(int l, int c) {
+		towerTaker_defense_core(l-1,c,-1,0);
+		towerTaker_defense_core(l+1,c,1,0);
+		towerTaker_defense_core(l,c-1,0,-1);
+		towerTaker_defense_core(l,c+1,0,1);
+	}
+	
+	
+	public void towerTaker_defense_core(int l, int c, int dl, int dc){
+		if(game.isAttackTower(l, c)){
+			if(isDefTowerHelper(l+dl, c+dc)) // Si deux cases plus loin nous avons un allié de circonstance pour la défense
+				game.setContent(CellContent.EMPTY,l,c); // on enleve le pion ennemi
+			else  
+				testSurround(l, c); // Sinon on vérifie que le coup joué  bloque totalement le pion ennemi
+				
+		}
+	}
+	
+	public void towerTaker_attack_core(int l, int c, int dl, int dc){
+		if(game.isDefenseTower(l, c)){ // Si la case contient une tour défensive
+			if(isAttTowerHelper(l+dl, c+dc)) // Si deux cases plus loin nous avons un allié de circonstance pour l'attaque
+				game.setContent(CellContent.EMPTY,l,c); // on enleve le pion ennemi
+			else 
+				testSurround(l, c);  // Sinon on vérifie que le coup joué  bloque totalement le pion ennemi
+		}
+		else if(game.isTheKing(l,c)) // Si la case contient le roi
+				testSurround(l, c);  // On vérifie que le coup joué  bloque totalement le roi ennemi
+	}
+	
+	
+	public void testSurround(int l, int c) {
+		if(isSurrounded(new Couple<Integer, Integer>(l, c),l,c)) { // Si le pion est encerclé
+			game.setContent(CellContent.EMPTY,l,c); // On enlève le pion
+			if(!visited.isEmpty()) {
+				for(Couple<Integer, Integer> cell : visited) { 
+					game.setContent(CellContent.EMPTY,cell.getFirst(),cell.getSecond()); // // Et on enlève les pions encerclés avec le précédent
+				}
+			}
+		}
+		visited.clear();
+	}
+	
+	public boolean isSurrounded(Couple<Integer, Integer> c, int i, int j) {
+		List<Couple<Integer, Integer>> allyCells = new ArrayList<>();
+		boolean surrounded = true;
+		int counter_obstacle = 0;
+		Couple<Integer , List<Couple<Integer, Integer>>> var = new Couple<Integer, List<Couple<Integer, Integer>>>(counter_obstacle, allyCells);
+		
+		switch(game.getGrid()[i][j]) {
+			case DEFENSE_TOWER:
+				var = checkneighbour_defense(var.getSecond(), var.getFirst(), i-1, j);
+				var = checkneighbour_defense(var.getSecond(), var.getFirst(), i+1, j);
+				var = checkneighbour_defense(var.getSecond(), var.getFirst(), i, j-1);
+				var = checkneighbour_defense(var.getSecond(), var.getFirst(), i, j+1);
+				break;
+
+			case KING:
+				var = checkneighbour_defense(var.getSecond(), var.getFirst(), i-1, j);
+				var = checkneighbour_defense(var.getSecond(), var.getFirst(), i+1, j);
+				var = checkneighbour_defense(var.getSecond(), var.getFirst(), i, j-1);
+				var = checkneighbour_defense(var.getSecond(), var.getFirst(), i, j+1);
+				break;
+
+			case ATTACK_TOWER:
+				var = checkneighbour_attack(var.getSecond(), var.getFirst(), i-1, j);
+				var = checkneighbour_attack(var.getSecond(), var.getFirst(), i+1, j);
+				var = checkneighbour_attack(var.getSecond(), var.getFirst(), i, j-1);
+				var = checkneighbour_attack(var.getSecond(), var.getFirst(), i, j+1);
+				break;
+				
+			default: break;	
+		}
+		
+		counter_obstacle = var.getFirst();
+		if(counter_obstacle == 4) { // Le pion ne peut plus se déplacer
+				if(var.getSecond().size()!=0) { // Si il a des alliés autour de lui, on vérifie si eux mêmes sont bloqués 
+					if(!visited.contains(c)) {
+						visited.add(c);
+					}
+					for(Couple<Integer, Integer> cell : visited) {
+						allyCells.remove(cell);
+					}
+					for(Couple<Integer, Integer> cell : allyCells) {
+						if(!isSurrounded(new Couple<Integer, Integer>(cell.getFirst(), cell.getSecond()),cell.getFirst(),cell.getSecond())) {
+							surrounded = false;
+						}
+					}
+					return surrounded;	
+				}
+				return true; 
+		}
+		return false;		
+	}
+
+	// vérifie de quelle nature est la case voisine 
+	public Couple<Integer, List<Couple<Integer, Integer>>> checkneighbour_attack(List<Couple<Integer, Integer>> allyCells, int counter_obstacle, int i, int j){
+			if(!game.isValid(i, j)) 
+				counter_obstacle++;
+			
+			else if(isDefTowerHelper(i,j)) 
+				counter_obstacle++;
+			
+			else if(game.isAttackTower(i,j)){
+				allyCells.add(new Couple<Integer, Integer>(i, j));
+				counter_obstacle++;
+			}
+			
+			return new Couple<Integer, List<Couple<Integer, Integer>>>(counter_obstacle, allyCells);
+	}
+	
+	// vérifie de quelle nature est la case voisine 
+	public Couple<Integer, List<Couple<Integer, Integer>>> checkneighbour_defense(List<Couple<Integer, Integer>> allyCells, int counter_obstacle, int i, int j){
+			if(!game.isValid(i, j)) 
+				counter_obstacle++;
+			
+			else if(isAttTowerHelper(i,j)) 
+				counter_obstacle++;	
+			
+			else if(isDefenseAlly(i, j)) {
+				allyCells.add(new Couple<Integer, Integer>(i, j));
+				counter_obstacle++;
+			}
+			
+			return new Couple<Integer, List<Couple<Integer, Integer>>>(counter_obstacle, allyCells);
+	}
+	
+	
+	
+	
+	
+	public boolean isDefTowerHelper(int i, int j) {
+		if((game.isTheKingPlace(i,j) && !game.isOccupied(i,j)) || game.isDefenseTower(i,j) || game.isGate(i,j)) 
+			return true;
+		else 
+			return false;
+	}
+	
+	
+	
+	public boolean isAttTowerHelper(int i, int j) {
+		if((game.isTheKingPlace(i,j) && !game.isOccupied(i,j)) || game.isAttackTower(i,j) || game.isGate(i,j)) 
+			return true;
+		else 
+			return false;
+	}
+	
+	public boolean isDefenseAlly(int i ,int j) {
+		if(game.isTheKing(i, j) || game.isDefenseTower(i, j))
+			return true;
+		else
+			return false;
+	}
+}
